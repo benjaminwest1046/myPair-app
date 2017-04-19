752a6d9c1b25cbfa510299b5028ebe62a2042512
Benjamin West <benjamin_west@homedepot.com>
2017-04-18 20:58:14 -0400

    Working through pair selection logic

--- client/controllers/pairs.controller.js
+++ client/controllers/pairs.controller.js
@@ -6,38 +6,47 @@
     .controller('pairCtrl', pairCtrl);
 
     function pairCtrl(_pairService_, $http, $state) {
-      var pairService = _pairService_;
-      var vmPairs = this;
-      vmPairs.newPair = newPair;
-    vmPairs.pair = {
-      date: '',
-      pairs: []
-    }
-    vmPairs.pairs = [];
+        var pairService = _pairService_;
+        var vmPairs = this;
+        vmPairs.newPair = newPair;
+        vmPairs.handleClick = handleClick;
+        vmPairs.developers = [
+         "Benjamin", "Chad"
+        ]
+        
+        vmPairs.pair = {
+          date: '',
+          pairs: []
+        }
+        vmPairs.pairs = [];
 
-    init();
-    function init() {
-      getPairs().then(function(pairs) {
-        vmPairs.pairs = pairs;
-      });
-    }
+        init();
 
-    function getPairs() {
-      return pairService.getPairs()
-      .then(res => {
-        return res.data;
-      })
-    }
+        function init() {
+          getPairs().then(function(pairs) {
+            vmPairs.pairs = pairs;
+          });
+        }
 
-    function newPair() {
-      vmPairs.pair.pairs.push(vmPairs.pairs)  
-              console.log(vmPairs.pair)
+        function getPairs() {
+          return pairService.getPairs()
+          .then(res => {
+            return res.data;
+          })
+        }
 
-      pairService.createPair(vmPairs.pair).then(function(res) {
-        $state.go('pair')
-      })
-    }
+        function newPair() {
+            vmPairs.pair.pairs.push(vmPairs.pairs)  
+            console.log(vmPairs.pair)
 
+          pairService.createPair(vmPairs.pair).then(function(res) {
+            $state.go('pair')
+          })
+        }
 
+        function handleClick(developer) {
+            developer.selected = !developer.selected;
+            console.log(vmPairs.developers);
+        }
     }
 })();
